0e9f3b57ca67693aac349c6e1dc8fbe91c4bb2e0
mtthss <matteo.hessel@mail.polimi.it>
2015-02-13 18:12:23 +0000

    initial commit

--- Preprocessing/loadData.py
+++ Preprocessing/loadData.py
@@ -1,31 +1,35 @@
 
-# imports
-from csv import reader
+# Imports
 import time
+from csv import reader, writer
 from gensim.models.word2vec import Word2Vec
-from gensim.corpora import WikiCorpus
+from nltk import word_tokenize
+from nltk.corpus import stopwords
 
-# utility variables
+# Utility variables
 i=1
-limit=1000000
+limit=10
 trainset={}
 
-# word2vec parameters
-num_features = 300
-min_word_count = 2
-num_workers = 4
-context = 10
-downsampling = 1e-3
-
-# paths
-model_name = "../TrainedModels/word_vect_model"
+# Paths
+model_name = '../Data/GoogleNews-vectors-negative300.bin'
 train_path = "../Data/train.tsv"
 
-# load data
+# Load google pre-trained word vectors
+print '\n------------------'
+print 'Loading word2vec model...'
+
+start_time = time.time()
+model = Word2Vec.load_word2vec_format(model_name, binary=True)  # C binary format
+model_time = time.time() - start_time
+
+# Load competition data
 print '\n------------------'
 print 'Loading competition data...'
-with open(train_path) as tsv:
 
+start_time = time.time()
+
+with open(train_path) as tsv:
     # intitialize reader and skip header
     reader = reader(tsv, dialect="excel-tab")
     reader.next()
@@ -41,54 +45,32 @@ with open(train_path) as tsv:
         if i>limit:
             break
 
-# loading wikipedia corpus
-# print '\n------------------'
-# print "Loading additional data..."
-# corpus = WikiCorpus('enwiki-20100622-pages-articles.xml.bz2',dictionary=True)
+reviewLoading_time = time.time() - start_time
 
-# train word2vec on the training set
+# Vectorize sentences
 print '\n------------------'
-print "Training model..."
+print 'Vectorizing sentences'
+
 start_time = time.time()
+cachedStopWords = stopwords.words("english")
 
-sentences = []
-for key, val in trainset.iteritems():
-    sentences.append([key,val[1]])
+with open('../transformedData.csv', 'wb') as csvout:
+    my_writer = writer(csvout, delimiter=',')
+    for i in range(1,len(trainset)):
+        curr_sent = trainset[str(i)][1]
+        for word in word_tokenize(curr_sent):
+            my_writer.writerow(model[word])
 
-model = Word2Vec(sentences, workers=num_workers, size=num_features, min_count = min_word_count, window = context, sample = downsampling)
+sentVect_time = time.time() - start_time
 
-# logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-# max_sentence = -1
-# def generate_lines():
-#     for index, text in enumerate(corpus.get_texts()):
-#         if index < max_sentence or max_sentence==-1:
-#             yield text
-#         else:
-#             break
-# model = Word2Vec()
-# model.build_vocab(generate_lines())
-# model.train(generate_lines(),chunksize=500)
-
-running_time = time.time() - start_time
-
-# make more mem efficient and store
-model.init_sims(replace=True)
-model.save(model_name)
-
-# test
+# Profiling
 print '\n------------------'
-print "Testing semantic relations...\n"
-# print model.most_similar("movie")
-# print model.doesnt_match("france england germany berlin".split())
-# print model.most_similar("man")
+print "Printing...\n"
+print trainset['1']
+print "Loading word2vec model: %f seconds" % model_time
+print "Loading movie reviews: %f seconds" % reviewLoading_time
+print "Vectorizing sentences: %f seconds" % sentVect_time
 
-# random prints
-print '\n------------------'
-print "Printing data...\n"
-print "dictionary length: %d" % len(trainset)
-print "sentence length: %d" % len(sentences)
-print "Running time: %f seconds" % running_time
-
-# ending
+# Ending
 print '\n------------------'
 print "Ending..."
