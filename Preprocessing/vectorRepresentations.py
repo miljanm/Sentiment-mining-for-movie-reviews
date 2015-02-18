60422b0b5cd4332c3f3add9e8bd17d1ddf05a46f
mtthss <matteo.hessel@mail.polimi.it>
2015-02-18 00:09:55 +0000

    added 2 possible word to sentence policies

--- Preprocessing/vectorRepresentations.py
+++ Preprocessing/vectorRepresentations.py
@@ -1,6 +1,19 @@
 
 import numpy
 
+
+# derive simple bagOfWords representation of a review
+def buildBagOfWords():
+
+    #vectorizer = CountVectorizer(analyzer="word",tokenizer=None,preprocessor=None,stop_words=None,max_features=5000)
+    # fit_transform() does two functions: First, it fits the model
+    # and learns the vocabulary; second, it transforms our training data
+    # into feature vectors. The input to fit_transform should be a list of
+    # strings.
+    #train_data_features = vectorizer.fit_transform(clean_reviews)
+    print 'not yet implemented'
+
+
 # derive sentence representation have sum of word vectors
 def buildSentVecAsSum(clean_sent, model):
 
@@ -14,6 +27,7 @@ def buildSentVecAsSum(clean_sent, model):
 
     return temp
 
+
 # derive sentence representation as average of word vectors
 def buildSentVecAsAverage(clean_sent, model):
 
@@ -26,7 +40,7 @@ def buildSentVecAsAverage(clean_sent, model):
             N = N+1
         except:
             pass
-
-    temp = temp/N
+    if N>0:
+        temp = temp/N
 
     return temp
