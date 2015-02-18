b3cd6bd3a34cd83171406ca51013f113b55f1017
mtthss <matteo.hessel@mail.polimi.it>
2015-02-18 21:49:44 +0000

    added 2 possible word to sentence policies

--- PostProcessing/majorityVoting.py
+++ PostProcessing/majorityVoting.py
@@ -4,6 +4,7 @@
 ###########
 import csv
 from os import walk
+import time
 import numpy
 
 
@@ -13,10 +14,15 @@ import numpy
 my_path = '../Data/Ensemble/'
 singleVote = False
 neighborWeight = 0.2
+first_id = 156061
 
 ##############################
 # Collect Models to Ensemble #
 ##############################
+print '\n------------------'
+print "Collecting...\n"
+start_time = time.time()
+
 fileList = []
 
 for (dirpath, dirnames, filenames) in walk(my_path):
@@ -28,10 +34,15 @@ reads = [csv.reader(handler, delimiter = ',') for handler in csvins]
 
 test_samples = sum(1 for line in open(fileList[0]))
 
+collect_time = time.time() - start_time
 
 #################
 # Extract Votes #
 #################
+print '\n------------------'
+print "Extracting...\n"
+start_time = time.time()
+
 reader_idx = 0
 votes = numpy.zeros((test_samples, 5, len(fileList)))
 
@@ -53,38 +64,41 @@ for read in reads:
 
     reader_idx = reader_idx + 1
 
+extract_time = time.time() - start_time
 
 #################
 # Combine Votes #
 #################
-#with open('../Data/ensemplePredictions.csv', 'wb') as csvout:
+print '\n------------------'
+print "Combining...\n"
+start_time = time.time()
+row_idx = 0
 
-#    write_out = csv.writer(csvout, delimiter = ',')
-#    write_out.writerow(['PhraseId', 'Sentiment'])
+with open('../Data/ensemblePredictions.csv', 'wb') as csvout:
 
-"""
-with open('ensemplePredictions.csv', 'wb') as csvout:
+    write_out = csv.writer(csvout, delimiter = ',')
+    write_out.writerow(['PhraseId', 'Sentiment'])
 
-	writer = csv.writer(csvout, delimiter=',')
-	with open('f1.csv', 'rb') as csvin1:
-		with open('f2.csv', 'rb') as csvin2:
-			with open('f3.csv', 'rb') as csvin3:
-				with open('f4.csv', 'rb') as csvin4:
+    summed_votes = numpy.sum(votes, axis = 2)
+    majority_pred = numpy.argmax(summed_votes, axis = 1)
 
-					reader1 = csv.reader(csvin1, delimiter=',')
-					reader2 = csv.reader(csvin2, delimiter=',')
-					reader3 = csv.reader(csvin3, delimiter=',')
-					reader4 = csv.reader(csvin4, delimiter=',')
-					reader1.next()
-					reader2.next()
-					reader3.next()
-					reader4.next()
-					writer.writerow('id,click')
+    for row in majority_pred.tolist():
+        write_out.writerow([first_id+row_idx,row])
+        row_idx = row_idx + 1
 
-					for row1 in reader1:
-						row2 = reader2.next()
-						row3 = reader3.next()
-						row4 = reader4.next()
-						row1[1] = str( (float(row1[1])*0.27+float(row2[1])*0.26+float(row3[1])*0.27+float(row4[1]))*0.20 )
-			 			writer.writerow(row1)
-                        """
+combine_time = time.time() - start_time
+
+#############
+# Profiling #
+#############
+print '\n------------------'
+print "Profiling...\n"
+print "Collecting models: %f seconds" % collect_time
+print "Extracting votes: %f seconds" % extract_time
+print "Combining votes: %f seconds" % combine_time
+
+##########
+# Ending #
+##########
+print '\n------------------'
+print 'Done!'
