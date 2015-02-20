2e99593fb08758dfcbab5942c1e8c639bf05d128
mtthss <matteo.hessel@mail.polimi.it>
2015-02-20 19:00:44 +0000

    added 2 possible word to sentence policies

--- Preprocessing/loadData.py
+++ Preprocessing/loadData.py
@@ -5,12 +5,15 @@
 import time
 import numpy
 import csv
+import cPickle
 
 from csv import reader, writer
 from gensim.models.word2vec import Word2Vec
 from nltk import word_tokenize
 from nltk.corpus import stopwords
-from vectorRepresentations import buildSentVecAsSum, buildSentVecAsAverage
+from vectorRepresentations import kmeansSentiment, buildSentVecAsSum, buildBagOfNgrams, buildSentVecAsAverage
+from pickle import load
+
 
 ###################
 # Choose Policies #
@@ -18,15 +21,22 @@ from vectorRepresentations import buildSentVecAsSum, buildSentVecAsAverage
 sentenceVect = buildSentVecAsSum
 #sentenceVect = buildSentVecAsAverage
 
+
 ##############
 # Initialize #
 ##############
 trainset={}
 testset={}
+freq_nGrams_path = '../Data/frequentNgrams.pkl'
 model_name = '../Data/GoogleNews-vectors-negative300.bin'
 train_path = '../Data/train.tsv'
 test_path = '../Data/test.tsv'
 first_id = 156061
+feat_word2vec = 301
+feat_kMeans = 5
+feat_nGrams = 270
+feat_total = feat_word2vec + feat_kMeans + feat_nGrams
+
 
 ########################################
 # Load google pre-trained word vectors #
@@ -37,6 +47,27 @@ print 'Loading word2vec model...'
 start_time = time.time()
 model = Word2Vec.load_word2vec_format(model_name, binary=True)  # C binary format
 model_time = time.time() - start_time
+
+
+###############################
+# Load kMeans and PCA models #
+###############################
+print '\n------------------'
+print 'Loading PCA and kMeans models...'
+
+kMeansModel = load(open("../Data/kMeans5.pkl", "rb" ))
+pcaModel = load(open("../Data/pcaMLE.pkl", "rb" ))
+
+
+########################
+# Load Frequent nGrams #
+########################
+print '\n------------------'
+print 'Loading Frequent nGrams...'
+
+f = open(freq_nGrams_path, 'r')
+freq_mGrams_list = cPickle.load(f)
+
 
 #########################
 # Load competition data #
@@ -73,6 +104,7 @@ with open(test_path) as tsv:
 
 reviewLoading_time_test = time.time() - start_time
 
+
 #######################
 # Vectorize sentences #
 #######################
@@ -82,12 +114,21 @@ print 'Vectorizing sentences'
 start_time = time.time()
 cachedStopWords = stopwords.words("english")
 
-data_matrix = numpy.zeros((len(trainset), 301))
-for i in xrange(0,len(trainset)):
-    curr_sent = word_tokenize(trainset[str(i+1)][1])
+data_matrix = numpy.zeros((len(trainset), feat_total))
+for i in xrange(0, len(trainset)):
+
+    # sentence
+    sent = trainset[str(i+1)][1]
+    curr_sent = word_tokenize(sent)
     clean_sent = [word for word in curr_sent if word not in cachedStopWords]
-    data_matrix[i,0] = int(trainset[str(i+1)][2])
-    data_matrix[i,1:] = sentenceVect(clean_sent, model)
+    # class
+    data_matrix[i, 0] = int(trainset[str(i+1)][2])
+    # word2vec representation
+    data_matrix[i, 1:feat_word2vec+1] = sentenceVect(clean_sent, model)
+    # clustering representation
+    data_matrix[i, feat_word2vec+1:feat_word2vec+feat_kMeans+1] = kmeansSentiment(sent, kMeansModel, pcaModel, model)
+    # nGram representation
+    data_matrix[i, feat_word2vec+feat_kMeans+1:feat_word2vec+feat_kMeans+feat_nGrams+1] = buildBagOfNgrams(sent,freq_mGrams_list)
 
 with open('../Data/transformedData.csv', 'wb') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
@@ -97,12 +138,21 @@ with open('../Data/transformedData.csv', 'wb') as csvfile:
 sentVect_time_train = time.time() - start_time
 start_time = time.time()
 
-data_matrix = numpy.zeros((len(testset), 301))
+data_matrix = numpy.zeros((len(testset), feat_total))
 for i in xrange(first_id,first_id+len(testset)):
-    curr_sent = word_tokenize(testset[str(i)][1])
+
+    # sentence
+    sent = testset[str(i)][1]
+    curr_sent = word_tokenize(sent)
     clean_sent = [word for word in curr_sent if word not in cachedStopWords]
+    # class
     data_matrix[i-first_id, 0] = 2
-    data_matrix[i-first_id, 1:] = sentenceVect(clean_sent, model)
+    # word2vec representation
+    data_matrix[i, 1:feat_word2vec+1] = sentenceVect(clean_sent, model)
+    # clustering representation
+    data_matrix[i, feat_word2vec+1:feat_word2vec+feat_kMeans+1] = kmeansSentiment(sent, kMeansModel, pcaModel, model)
+    # nGram representation
+    data_matrix[i, feat_word2vec+feat_kMeans+1:feat_word2vec+feat_kMeans+feat_nGrams+1] = buildBagOfNgrams(sent,freq_mGrams_list)
 
 with open('../Data/transformedTestData.csv', 'wb') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
@@ -110,6 +160,7 @@ with open('../Data/transformedTestData.csv', 'wb') as csvfile:
         writer.writerow(row)
 
 sentVect_time_test = time.time() - start_time
+
 
 #############
 # Profiling #
@@ -121,6 +172,7 @@ print "Loading train movie reviews: %f seconds" % reviewLoading_time_train
 print "Loading test movie reviews: %f seconds" % reviewLoading_time_test
 print "Vectorizing train sentences: %f seconds" % sentVect_time_train
 print "Vectorizing test sentences: %f seconds" % sentVect_time_test
+
 
 ##########
 # Ending #
